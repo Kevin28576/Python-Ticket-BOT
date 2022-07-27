0b27a4494bfb503a06fecd552c7ce975aa52581d
Kevin <89570292+Kevin28576@users.noreply.github.com>
2022-07-27 12:31:53 +0800

    初版

--- run-bot.py
+++ run-bot.py
@@ -1,3 +1,18 @@
+print('  ___  __    _______   ___      ___ ___  ________       ')
+print(' |\  \|\  \ |\  ___ \ |\  \    /  /|\  \|\   ___  \     ')
+print(' \ \  \/  /|\ \   __/|\ \  \  /  / | \  \ \  \\\ \  \    ')
+print('  \ \   ___  \ \  \_|/_\ \  \/  / / \ \  \ \  \\\ \  \   ')
+print('   \ \  \\\ \  \ \  \_|\ \ \    / /   \ \  \ \  \\\ \  \  ')
+print('    \ \__\\\ \__\ \_______\ \__/ /     \ \__\ \__\\\ \__\ ')
+print('     \|__| \|__|\|_______|\|__|/       \|__|\|__| \|__| ')
+print()
+print('原版由 HansHans135 製作。')
+print('Github:  https://github.com/HansHans135/ticket')
+print()
+print('此改版由 Krick#9685 所設計。')
+print()
+
+
 from discord_components import *
 import discord
 from datetime import datetime,timezone,timedelta
@@ -22,9 +37,6 @@ ULR = "http://backupmode.xyz/" #!! 請自行設定
 #線上看網址
 
 
-
-
-
 def startweb():
     app = Flask(__name__)
     @app.route("/", methods=['GET'])
@@ -43,12 +55,29 @@ t.start()
 @bot.event
 async def on_ready():
     print(f"機器人 {bot.user} 已上線!")
+    web_newpath = r'\web' 
+    if not os.path.exists(web_newpath):
+      print("未偵測到web資料夾，系統自動創建中...")
+      os.makedirs(web_newpath)
+      print("創建資料夾web完成~")
+    else:
+      print("偵測到web資料夾，繼續下一步啟動作業。")
+    data_newpath = r'\data' 
+    if not os.path.exists(data_newpath):
+      print("未偵測到data資料夾，系統自動創建中...")
+      os.makedirs(data_newpath)
+      print("創建資料夾data完成~")
+    else:
+      print("偵測到data資料夾，繼續下一步啟動作業。")
 
 
 @bot.command()
 async def ticket(ctx):
-    await ctx.send("有問題?\n創建一個私人頻道來聯繫管理員吧!",components = [Button(label = "開啟客服單", custom_id = "open",style=ButtonStyle.green)])
-    
+  if ctx.author.guild_permissions.manage_messages:
+    await ctx.send(jdata["Ticket_msg"],components = [Button(label = "開啟客服單", custom_id = "open",style=ButtonStyle.green)])
+  else:
+    await ctx.send("抱歉，您沒有權限進行此操作。")
+
 @bot.event
 async def on_button_click(interaction):
     def check(m):
@@ -93,11 +122,5 @@ async def on_message(message):
       dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
       now = dt2.strftime("%Y-%m-%d %H:%M:%S")
       filt.write(f'{now}|{message.author}:{message.content}\n')
-
-  if message.content == 'h!ticket':
-    if message.author.guild_permissions.manage_messages:
-        await message.channel.send("有問題?\n創建一個私人頻道來聯繫管理員吧!",components = [Button(label = "開啟客服單", custom_id = "open",style=ButtonStyle.green)])
-    else:
-      await message.channel.send("你沒有權限欸,要不要在你自己的伺服器試試看")
 
 bot.run(Token)
